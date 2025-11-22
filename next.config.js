87e3e78bcc34bb0ff91ea58bf521734313464f4c
masekaku <aguslistiyanto27@gmail.com>
2025-11-23 03:24:39 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Menggunakan Pages Router (default untuk struktur /pages)
+  poweredByHeader: false
+};
+
+module.exports = nextConfig;
