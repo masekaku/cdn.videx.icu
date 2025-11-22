ea22ff8851e2aa3da2e4ebc9936acbcf5d720075
masekaku <aguslistiyanto27@gmail.com>
2025-11-23 05:15:19 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const { withCloudflare } = require('@cloudflare/next-on-pages');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
 };
 
-module.exports = withCloudflare(nextConfig);
+module.exports = nextConfig;
