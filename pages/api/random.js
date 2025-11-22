0f177dcc0bbc074804611d0de7c1e6addf34354f
masekaku <aguslistiyanto27@gmail.com>
2025-11-23 04:28:50 +0700

    Update random.js

--- pages/api/random.js
+++ pages/api/random.js
@@ -1,14 +1,9 @@
-// pages/api/random.js
-import videos from '../../data/videos.json';
+import '../globals.css';
 
-export default function handler(req, res) {
-  if (!Array.isArray(videos) || videos.length === 0) {
-    res.status(500).json({ error: 'No videos available' });
-    return;
-  }
+export const runtime = 'edge';
 
-  const randomIndex = Math.floor(Math.random() * videos.length);
-  const randomVideo = videos[randomIndex];
-
-  res.status(200).json(randomVideo);
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
