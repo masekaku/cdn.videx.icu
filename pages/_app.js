30b7597b39288c86d09455b45754e8809f3f330e
masekaku <aguslistiyanto27@gmail.com>
2025-11-23 22:15:02 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import '../styles/global.css';
-// 1. Import komponen SpeedInsights
-import { SpeedInsights } from '@vercel/speed-insights/next';
+// Coba import variasi ini agar lebih kompatibel
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* 2. Render komponen halaman utama */}
       <Component {...pageProps} />
-      
-      {/* 3. Masukkan SpeedInsights di sini agar aktif di seluruh website */}
       <SpeedInsights />
     </>
   );
