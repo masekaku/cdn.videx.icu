c012e596a47af1fc61219eabda6bb1077f9cdab8
masekaku <aguslistiyanto27@gmail.com>
2025-11-23 22:20:32 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+// IMPORT PENTING: Mengambil CSS dari folder 'styles' di root
 import '../styles/global.css';
-// Coba import variasi ini agar lebih kompatibel
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
       <SpeedInsights />
     </>
